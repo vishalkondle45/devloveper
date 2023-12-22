c9adb77986476ba7029ce1d1f94ecc18e196d6cf
vishal-linux <vishal.kondle@gmail.com>
2023-12-23 03:13:54 +0530

    DevLovePer Icon button

---
+++ components/DevLovePer/DevLovePer.tsx
@@ -0,0 +1,28 @@
+import { Badge, ThemeIcon } from "@mantine/core";
+import { IconBraces } from "@tabler/icons-react";
+import Link from "next/link";
+
+const DevLovePer = () => {
+  return (
+    <>
+      <Badge
+        size="xl"
+        p={0}
+        style={{ cursor: "pointer" }}
+        component={Link}
+        href="/"
+        variant="transparent"
+        leftSection={
+          <ThemeIcon radius="xl">
+            <IconBraces size={18} />
+          </ThemeIcon>
+        }
+        styles={{ label: { textTransform: "capitalize" } }}
+      >
+        DevLovePer
+      </Badge>
+    </>
+  );
+};
+
+export default DevLovePer;
---
+++ components/DevLovePer/index.ts
@@ -0,0 +1,3 @@
+import DevLovePer from "./DevLovePer";
+
+export default DevLovePer;
