1cbc9974f209d628f32bd4cb907cfd8c819285b1
vishal-linux <vishal.kondle@gmail.com>
2023-12-26 19:39:22 +0530

    deployment issue - session type added to data

--- components/AuthButton/AuthButton.tsx
+++ components/AuthButton/AuthButton.tsx
@@ -3,6 +3,7 @@ import { Button } from "@mantine/core";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import LoggedIn from "../auth/LoggedIn";
+import { Session } from "next-auth";
 
 const AuthButton = () => {
   const { data, status } = useSession();
@@ -17,7 +18,7 @@ const AuthButton = () => {
     <>
       {isAuth ? (
         <>
-          <LoggedIn data={data} />
+          <LoggedIn data={data as Session} />
         </>
       ) : (
         <Button onClick={() => router.push("/auth/login")}>Login</Button>
