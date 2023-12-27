4f97c11efa6e988ab7bfe7296e647598d483e973
vishal-linux <vishal.kondle@gmail.com>
2023-12-27 23:35:58 +0530

    added - labels in sidebard

--- components/Navbar/Notes/NoteSidebar.tsx
+++ components/Navbar/Notes/NoteSidebar.tsx
@@ -1,15 +1,32 @@
 import { Button, Group } from "@mantine/core";
-import { IconNote, IconTags, IconTrash } from "@tabler/icons-react";
+import { useListState } from "@mantine/hooks";
+import { IconNote, IconTag, IconTags, IconTrash } from "@tabler/icons-react";
+import axios from "axios";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 import { NoteSidebarProps } from "./NoteSidebar.types";
 
 const NoteSidebar = ({ navigate }: NoteSidebarProps) => {
   const pathname = usePathname();
-  const list = [
+  const [list, handlers] = useListState([
     { path: "/notes", icon: IconNote, title: "Notes" },
     { path: "/notes/trash", icon: IconTrash, title: "Trash" },
     { path: "/notes/labels", icon: IconTags, title: "Labels" },
-  ];
+  ]);
+
+  const getLabels = async () => {
+    await axios.get("/api/notes/labels").then((res) => {
+      let labels = res.data.map(({ _id, title }: any) => ({
+        path: "/" + _id,
+        title,
+      }));
+      handlers.append(...labels);
+    });
+  };
+
+  useEffect(() => {
+    getLabels();
+  }, []);
 
   return (
     <Group mt="xs" gap="xs">
@@ -17,7 +34,7 @@ const NoteSidebar = ({ navigate }: NoteSidebarProps) => {
         <Button
           key={item.path}
           variant={pathname === item.path ? "filled" : "outline"}
-          leftSection={<item.icon />}
+          leftSection={item.icon ? <item.icon /> : <IconTag />}
           onClick={() => navigate(item.path)}
           fullWidth
         >
