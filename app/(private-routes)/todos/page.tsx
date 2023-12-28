df331a611a4c8801dda85eb0aac7aa6badc8a826
vishal-linux <vishal.kondle@gmail.com>
2023-12-28 23:51:13 +0530

    added: todos ui page and layout

---
+++ app/(private-routes)/todos/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import BreadcrumbsComp from "@/components/Navbar/Breadcrumbs";
+import {
+  Center,
+  Container,
+  Group,
+  LoadingOverlay,
+  Stack,
+  Text,
+  ThemeIcon,
+  rem,
+} from "@mantine/core";
+import { IconNote } from "@tabler/icons-react";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+const Page = () => {
+  const { status } = useSession();
+  const [todos, setTodos] = useState<any[] | null>(null);
+
+  const getTodos = async () => {
+    const res = await axios.get("/api/todos");
+    setTodos(res.data);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const breadcrumbs = [
+    { title: "Home", href: "/" },
+    { title: "Todos", href: "/todos" },
+  ];
+
+  if (status === "loading" || !todos) {
+    return <LoadingOverlay visible />;
+  }
+
+  return (
+    <Container my="md" size="md">
+      <BreadcrumbsComp breadcrumbs={breadcrumbs} />
+      <Group justify="space-between">
+        <Text fz={rem(40)} fw={700}>
+          Todos
+        </Text>
+      </Group>
+      {!todos?.length ? (
+        <Center h={500}>
+          <Stack align="center">
+            <ThemeIcon color="gray" variant="transparent" size={rem(100)}>
+              <IconNote style={{ width: rem(100), height: rem(100) }} />
+            </ThemeIcon>
+            <Text size="xl">No todos created yet.</Text>
+          </Stack>
+        </Center>
+      ) : (
+        <></>
+      )}
+    </Container>
+  );
+};
+
+export default Page;
