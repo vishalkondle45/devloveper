7fd60b85bdd440e5646156c19522babd70270bdc
vishal-linux <vishal.kondle@gmail.com>
2024-01-06 22:27:25 +0530

    added: scale issue on textfield

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,10 @@ export default function RootLayout({
             sizes="any"
           />
           <ColorSchemeScript />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
         </head>
         <body>
           <MantineProvider theme={theme}>
