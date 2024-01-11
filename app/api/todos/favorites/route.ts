02d0437139532256fbd3fb1d888012c643106915
vishal-linux <vishal.kondle@gmail.com>
2024-01-11 12:52:44 +0530

    feature: favorites page in todos

---
+++ app/api/todos/favorites/route.ts
@@ -0,0 +1,21 @@
+import startDb from "@/lib/db";
+import TodoModel from "@/models/Todo";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/authOptions";
+
+export const GET = async (req: NextRequest): Promise<any> => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json(
+      { error: "You are not authenticated!" },
+      { status: 401 }
+    );
+  }
+  await startDb();
+  const todos = await TodoModel.find({
+    user: session.user?._id,
+    favorite: true,
+  });
+  return NextResponse.json(todos, { status: 200 });
+};
